ed6ea7bd993282ac56d42c4d656b7de9be1556c7
Stephen Reyes <sjreyes1@yahoo.com>
2018-10-29 13:39:31 -0700

    AVfountation and assosicated requirements to use backCamera for photos.

--- vision-app-dev/vision-app-dev/Controller/CameraVC.swift
+++ vision-app-dev/vision-app-dev/Controller/CameraVC.swift
@@ -5,10 +5,14 @@
 //  Created by Stephen Reyes on 10/29/18.
 //  Copyright © 2018 Stephen Reyes. All rights reserved.
 //
-
 import UIKit
+import AVFoundation
 
 class CameraVC: UIViewController {
+    
+    var caputureSession: AVCaptureSession!
+    var cameraOutput: AVCapturePhotoOutput!
+    var previewLayer: AVCaptureVideoPreviewLayer!
 
     @IBOutlet weak var cameraView: UIView!
     @IBOutlet weak var identificationLbl: UIView!
@@ -22,7 +26,36 @@ class CameraVC: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
-
+    override func viewDidAppear(_ animated: Bool) {
+        super .viewDidAppear(animated)
+        previewLayer.frame = cameraView.bounds
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        caputureSession = AVCaptureSession()
+        caputureSession.sessionPreset = AVCaptureSession.Preset.hd1920x1080
+        
+        let backCamera = AVCaptureDevice.default(for: AVMediaType.video)
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: backCamera!)
+            if caputureSession.canAddInput(input) == true {
+                caputureSession.addInput(input)
+            }
+            cameraOutput = AVCapturePhotoOutput()
+            if caputureSession.canAddOutput(cameraOutput) == true {
+                caputureSession.addOutput(cameraOutput!)
+                
+                previewLayer = AVCaptureVideoPreviewLayer(session: caputureSession)
+                previewLayer.videoGravity = AVLayerVideoGravity.resizeAspect
+                previewLayer.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+                
+                cameraView.layer.addSublayer(previewLayer!)
+                caputureSession.startRunning()
+            }
+        } catch {
+            debugPrint(error)
+        }
+    }
 }
 
